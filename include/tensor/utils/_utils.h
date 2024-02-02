12f242a53c70b11009dc24ef944deb606e49a77e
Sapiens_homo <laizhengbao@icloud.com>
2024-02-02 22:54:58 +0800

    fix: code style

--- include/tensor/utils/_utils.h
+++ include/tensor/utils/_utils.h
@@ -24,49 +24,9 @@ extern "C" {
 #define _Tensor_new_zeros(Type) _Tensor_new_zeros_##Type
 
 #include "transtype.h"
-#define Tensor_to(T, Type2) _Tensor_to(\
-		__builtin_choose_expr(\
-			__builtin_types_compatible_p (__typeof__(T), i8), i8,\
-			__builtin_choose_expr (\
-				__builtin_types_compatible_p (__typeof__(T), u8), u8,\
-				__builtin_choose_expr (\
-					__builtin_types_compatible_p (__typeof__(T), i16), i16,\
-					__builtin_choose_expr (\
-						__builtin_types_compatible_p (__typeof__(T), u16), u16,\
-						__builtin_choose_expr (\
-							__builtin_types_compatible_p (__typeof__(T), i32), i32,\
-							__builtin_choose_expr (\
-								__builtin_types_compatible_p (__typeof__(T), u32), u32,\
-								__builtin_choose_expr (\
-									__builtin_types_compatible_p (__typeof__(T), i64), i64,\
-									__builtin_choose_expr (\
-										__builtin_types_compatible_p (__typeof__(T), u64), u64,\
-										__builtin_choose_expr (\
-											__builtin_types_compatible_p (__typeof__(T), i128), i128,\
-											__builtin_choose_expr (\
-												__builtin_types_compatible_p (__typeof__(T), u128), u128,\
-												__builtin_choose_expr (\
-													__builtin_types_compatible_p (__typeof__(T), f16), f16,\
-													__builtin_choose_expr (\
-														__builtin_types_compatible_p (__typeof__(T), f32), f32,\
-														__builtin_choose_expr (\
-															__builtin_types_compatible_p (__typeof__(T), f64), f64,\
-															__builtin_choose_expr (\
-																__builtin_types_compatible_p (__typeof__(T), f80), f80,\
-																__builtin_choose_expr (\
-																	__builtin_types_compatible_p (__typeof__(T), f128), f128,\
-																	__builtin_choose_expr( __ENABLE_COMPLEX__,\
-																		__builtin_types_compatible_p (__typeof__(T), cf16), cf16,\
-																		__builtin_choose_expr (\
-																			__builtin_types_compatible_p (__typeof__(T), cf32), cf32,\
-																			__builtin_choose_expr (\
-																				__builtin_types_compatible_p (__typeof__(T), cf64), cf64,\
-																				__builtin_choose_expr (\
-																					__builtin_types_compatible_p (__typeof__(T), cf80), cf80,\
-																					__builtin_choose_expr (\
-																						__builtin_types_compatible_p (__typeof__(T), cf128), cf128,\
-																						(void)0 )))), (void)0 )\
-																						))))))))))))))), Type2) (T)
+	/*
+	 * Tensor_to(T, Type)
+	 */
 
 #define _Tensor_to_dense(Type) _Tensor_to_dense_##Type
 #define _Tensor_to_sparse(Type) _Tensor_to_sparse_##Type
