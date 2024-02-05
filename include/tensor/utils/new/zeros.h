848fed1c51a7680eecc92a96c7935890fecb6e94
Sapiens_homo <laizhengbao@icloud.com>
2024-02-05 11:36:14 +0800

    fix: implement: Tensor_new_zeros, Tensor_new_ones

--- include/tensor/utils/new/zeros.h
+++ include/tensor/utils/new/zeros.h
@@ -10,43 +10,10 @@ extern "C" {
 #include "../../type.h"
 
 	/*
-	 *                  !!! WARNING !!!
-	 * THIS FUNCTION WOULD BE A ALIAS OF Tensor_zeros()
-	 */
-
-	/*
 	 * Tensor, dim of new tensor, shape of new tensor
 	 */
 
-	Tensor(bool) _Tensor_new_zeros_bool (Tensor(bool), size_t, size_t *);
-	Tensor(i8) _Tensor_new_zeros_i8 (Tensor(i8), size_t, size_t *);
-	Tensor(u8) _Tensor_new_zeros_u8 (Tensor(u8), size_t, size_t *);
-	Tensor(i16) _Tensor_new_zeros_i16 (Tensor(i16), size_t, size_t *);
-	Tensor(u16) _Tensor_new_zeros_u16 (Tensor(u16), size_t, size_t *);
-	Tensor(i32) _Tensor_new_zeros_i32 (Tensor(i32), size_t, size_t *);
-	Tensor(u32) _Tensor_new_zeros_u32 (Tensor(u32), size_t, size_t *);
-	Tensor(i64) _Tensor_new_zeros_i64 (Tensor(i64), size_t, size_t *);
-	Tensor(u64) _Tensor_new_zeros_u64 (Tensor(u64), size_t, size_t *);
-	Tensor(i128) _Tensor_new_zeros_i128 (Tensor(i128), size_t, size_t *);
-	Tensor(u128) _Tensor_new_zeros_u128 (Tensor(u128), size_t, size_t *);
-
-	Tensor(f16) _Tensor_new_zeros_f16 (Tensor(f16), size_t, size_t *);
-	Tensor(f32) _Tensor_new_zeros_f32 (Tensor(f32), size_t, size_t *);
-	Tensor(f64) _Tensor_new_zeros_f64 (Tensor(f64), size_t, size_t *);
-	Tensor(f80) _Tensor_new_zeros_f80 (Tensor(f80), size_t, size_t *);
-	Tensor(f128) _Tensor_new_zeros_f128 (Tensor(f128), size_t, size_t *);
-
-#if __ENABLE_COMPLEX__
-
-	Tensor(cf16) _Tensor_new_zeros_cf16 (Tensor(cf16), size_t, size_t *);
-	Tensor(cf32) _Tensor_new_zeros_cf32 (Tensor(cf32), size_t, size_t *);
-	Tensor(cf64) _Tensor_new_zeros_cf64 (Tensor(cf64), size_t, size_t *);
-	Tensor(cf80) _Tensor_new_zeros_cf80 (Tensor(cf80), size_t, size_t *);
-	Tensor(cf128) _Tensor_new_zeros_cf128 (Tensor(cf128), size_t, size_t *);
-
-#endif
-
-#define Tensor_new_zeros(T) _Tensor_new_zeros(\
+#define Tensor_new_zeros(T, dim, shape) _Tensor_zeros(\
 		__builtin_choose_expr(\
 			__builtin_types_compatible_p (__typeof__(T), Tensor(i8)), i8,\
 			__builtin_choose_expr (\
@@ -88,7 +55,7 @@ extern "C" {
 																					__builtin_choose_expr (\
 																						__builtin_types_compatible_p (__typeof__(T), Tensor(cf128)), cf128,\
 																						(void)0 )))), (void)0 )\
-																						)))))))))))))))) (T)
+																						)))))))))))))))) (dim, shape, T.require_grad)
 
 #ifdef __cplusplus
 }
