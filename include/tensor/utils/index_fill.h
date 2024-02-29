5c46873037199def7e894cd8b1b0c1202430a813
Sapiens_homo <laizhengbao@icloud.com>
2024-02-29 04:30:24 +0000

    feat: declaration: Tensor_fill

---
+++ include/tensor/utils/index_fill.h
@@ -0,0 +1,17 @@
+
+#ifndef __TENSOR_UTILS_INDEX_FILL_H__
+#define __TENSOR_UTILS_INDEX_FILL_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "_utils.h"
+#include "../type.h"
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
